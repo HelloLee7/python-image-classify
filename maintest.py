eb550c2436f248c98600a6292c93fb77a976a295
HelloLee7 <jhelee920@gmail.com>
2024-10-30 14:04:46 +0900

    update

--- maintest.py
+++ maintest.py
@@ -5,7 +5,6 @@ import numpy as np
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-# Load the model
 model = load_model("C:\\Users\\201-23\\Downloads\\opencv-install\\python-image-classify\\keras_model.h5", compile=False)
 class_names = open("C:\\Users\\201-23\\Downloads\\opencv-install\\python-image-classify\\labels.txt", "r").readlines()
 
@@ -17,31 +16,25 @@ while True:
     ret, image = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
-    image_resized = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-
-    # Make the image a numpy array and reshape it to the model's input shape.
-    image_input = np.asarray(image_resized, dtype=np.float32).reshape(1, 224, 224, 3)
-
-    # Normalize the image array
-    image_input = (image_input / 127.5) - 1
-
-    # Predicts the model
-    prediction = model.predict(image_input)
-    index = np.argmax(prediction)
-    class_name = class_names[index].strip()  # 클래스 이름에서 공백 제거
-    confidence_score = prediction[0][index]
-
-  
-    left, top = 50, 50   # 적절한 위치로 조정 가능
- 
-    label = f"{class_name} ({np.round(confidence_score * 1, 2)}%)"
-    cv2.putText(image, label, (left, top - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Show the image in a window
     cv2.imshow("Webcam Image", image)
 
+    # Make the image a numpy array and reshape it to the models input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predicts the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
     # Print prediction and confidence score
-    print("Class:", class_name, end="")
+    print("Class:", class_name[2:], end="")
     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
     # Listen to the keyboard for presses.
